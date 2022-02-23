a2ee22e9f0f74a1d550711e477e73b464ff66353
Makoto Yamada <woocie.rugby@gmail.com>
2022-02-23 16:07:48 +0900

    Initial commit

---
+++ .storybook/preview.tsx
@@ -0,0 +1,13 @@
+import { DecoratorFn, Parameters } from "@storybook/react";
+
+export const parameters: Parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators: DecoratorFn[] = [];
